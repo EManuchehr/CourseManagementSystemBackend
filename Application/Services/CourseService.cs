3c8799dfd60b7b9f1b4692a8366dbbd9dc1f47b1
Manuchehr1411 <manu1411@mail.ru>
2024-04-15 11:28:16 +0500

    refactoring course service

--- Application/Services/CourseService.cs
+++ Application/Services/CourseService.cs
@@ -1,33 +1,32 @@
 ﻿using Domain.Entities;
 using Domain.Interfaces.Repositories;
 
-namespace Application.Services
+namespace Application.Services;
+
+public class CourseService(IBaseRepository<Course> courseRepository) : IBaseService<Course>
 {
-    public class CourseService(IBaseRepository<Course> courseRepository) : IBaseService<Course>
+    public async Task<Course> GetAsync(Guid id, CancellationToken token = default)
     {
-        public async Task<Course> GetAsync(Guid id, CancellationToken token = default)
-        {
-            return await courseRepository.GetAsync(id, token);
-        }
+        return await courseRepository.GetAsync(id, token);
+    }
 
-        public async Task<IEnumerable<Course>> GetAllAsync(CancellationToken token = default)
-        {
-            return await courseRepository.GetAllAsync(token);
-        }
+    public async Task<IEnumerable<Course>> GetAllAsync(CancellationToken token = default)
+    {
+        return await courseRepository.GetAllAsync(token);
+    }
 
-        public async Task<Course> CreateAsync(Course instructor, CancellationToken token = default)
-        {
-            return await courseRepository.CreateAsync(instructor, token);
-        }
+    public async Task<Course> CreateAsync(Course instructor, CancellationToken token = default)
+    {
+        return await courseRepository.CreateAsync(instructor, token);
+    }
 
-        public async Task<bool> UpdateAsync(Course entity, CancellationToken token = default)
-        {
-            return await courseRepository.UpdateAsync(entity, token);
-        }
+    public async Task<bool> UpdateAsync(Course entity, CancellationToken token = default)
+    {
+        return await courseRepository.UpdateAsync(entity, token);
+    }
 
-        public async Task<bool> DeleteAsync(Course submission, CancellationToken token = default)
-        {
-            return await courseRepository.DeleteAsync(submission, token);
-        }
+    public async Task<bool> DeleteAsync(Course submission, CancellationToken token = default)
+    {
+        return await courseRepository.DeleteAsync(submission, token);
     }
 }
